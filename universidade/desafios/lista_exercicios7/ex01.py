dd936c3fe5031d5bca3ca4f63601a96183ea16c2
bennarthurpy <87674915+bennarthurpy@users.noreply.github.com>
2021-11-02 14:59:29 -0300

    Update ex01.py

--- universidade/desafios/lista_exercicios7/ex01.py
+++ universidade/desafios/lista_exercicios7/ex01.py
@@ -8,22 +8,13 @@ for y in range(5): #ler vetor 2
     vetor_2[y] = int(input(f"Digite o {y+ 1}° elemento do vetor 2:"))
 print(vetor_1)
 print(vetor_2)
+
 #vetor de elementos intercalados
-'''
-for i in range(5):
-    v3.append(v1[i])
-    v3.append(v2[i])
-'''
-x=0
-for z1 in range(9,2): #z1:0 , 2 , 4 , 6 , 8
-    vetor_3[z1] = vetor_1[x]
-    x+=1
-y= 0
-for z1 in range(1,10,2):#1 , 3 , 5, 7, 9
-    vetor_3[z1] = vetor_1[y]
-    y+=1
-    '''
-for z2 in range(1,10,2):#1, 3 , 5 , 7 , 9 
-    vetor_3[z2] = vetor_2[z2]
-    '''
+for i in range(0, 10, 2):#0,2,4,6,8
+    vetor_3[i] = vetor_1[i//2] # 0 , 1 , 2, 3, 4
+for j in range(1,11,2): #1,3,5,7,9
+    vetor_3[j] = vetor_2[(j-1)//2]# 0,1,2,3,4
+
+print(vetor_3)
+
 print(f"Vetor dos elementos intercalados:{vetor_3}")
